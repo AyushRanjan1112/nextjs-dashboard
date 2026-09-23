1abefc8eb5591b3307e817165521beb8506ea81a
Ayush <aayushra933@gmail.com>
2026-09-23 17:32:00 +0530

    Complete Chapter 3 - Optimizing Fonts and Images

--- app/ui/invoices/status.tsx
+++ app/ui/invoices/status.tsx
@@ -1,24 +1,27 @@
-import { CheckIcon, ClockIcon } from '@heroicons/react/24/outline';
-import clsx from 'clsx';
+import { CheckIcon, ClockIcon } from "@heroicons/react/24/outline";
+import clsx from "clsx";
 
 export default function InvoiceStatus({ status }: { status: string }) {
   return (
     <span
       className={clsx(
-        'inline-flex items-center rounded-full px-2 py-1 text-xs',
+        "inline-flex items-center rounded-full px-2 py-1 text-xs",
         {
-          'bg-gray-100 text-gray-500': status === 'pending',
-          'bg-green-500 text-white': status === 'paid',
+          // Implementation of the clsx library to toggle class names
+          // If status is paid, color: green
+          // If status is pending, color: gray
+          "bg-gray-100 text-gray-500": status === "pending",
+          "bg-green-500 text-white": status === "paid",
         },
       )}
     >
-      {status === 'pending' ? (
+      {status === "pending" ? (
         <>
           Pending
           <ClockIcon className="ml-1 w-4 text-gray-500" />
         </>
       ) : null}
-      {status === 'paid' ? (
+      {status === "paid" ? (
         <>
           Paid
           <CheckIcon className="ml-1 w-4 text-white" />
